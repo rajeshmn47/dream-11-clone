c3c3f4937990ec607a320a35d750c321a63fc21a
rajeshmn47 <rajeshmn47@gmail.com>
2022-05-09 00:55:44 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Register from './components/register';
+import { Routes, Route, Link, BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <>
+   <BrowserRouter>
+  
+        <Routes>
+       
+          <Route path='/register' element={<Register />} />
+     
+        </Routes>
+      
+      </BrowserRouter>
+ 
+ </>
   );
 }
 
---
+++ src/components/register.js
@@ -0,0 +1,9 @@
+export const Register=()=>{
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export default Register
