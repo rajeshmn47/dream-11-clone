91ce7706c6c50c89e1e531497e85e77a86bdd1dc
rajeshmn47 <rajeshmn47@gmail.com>
2022-05-09 05:29:51 +0530

    make it better

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,18 @@
-import {SportsCricketIcon,SportsSoccerIcon,SportsBasketballIcon,SportsHockeyIcon} from '@mui/icons-material';
+import SportsCricketIcon from '@mui/icons-material/SportsCricket';
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+import SportsBasketballIcon from '@mui/icons-material/SportsBasketball';
+import SportsHockeyIcon from '@mui/icons-material/SportsHockey';
+import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
 
 export const Home=()=>{
     return(
         <>
+           <div className='logintopbar'>
+       <EmojiEventsOutlinedIcon style={{marginRight:'1vw'}}/> 
+Dream 11
+        </div>
         <div className='hometop'>
-        <div className='hometopicon'>
+        <div className='hometopicon selectgame'>
             <SportsCricketIcon style={{color:'#C41E22'}}/>
             <h5>Cricket</h5>
             </div>
