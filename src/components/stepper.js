e1b626424f9abd8f6563603355cb58e1c8f71a51
rajeshmn47 <rajeshmn47@gmail.com>
2022-05-09 06:49:45 +0530

    make it better

--- src/components/stepper.js
+++ src/components/stepper.js
@@ -31,21 +31,10 @@ const CheckoutSteps = ({ activeStep }) => {
 
   return (
     <Fragment>
-      <Stepper alternativeLabel activeStep={activeStep} style={stepStyles}>
-        {steps.map((item, index) => (
-          <Step
-            key={index}
-            active={activeStep === index ? true : false}
-            completed={activeStep >= index ? true : false}
-          >
-            <StepLabel
-              style={{
-                color: activeStep >= index ? "tomato" : '#FFFFFF',
-              }}
-              icon={item.icon}
-            >
-              {item.label}
-            </StepLabel>
+      <Stepper alternativeLabel activeStep={1} connector={<QontoConnector />}>
+        {steps.map((a) => (
+          <Step key={a.label}>
+            <StepLabel StepIconComponent={QontoStepIcon}>{a.label}</StepLabel>
           </Step>
         ))}
       </Stepper>
