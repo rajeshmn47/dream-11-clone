1b58a2aec0bfebb985e850dac4125a6114767244
rajeshmn47 <rajeshmn47@gmail.com>
2024-02-12 23:39:55 +0530

    removing react native code

--- constants/matchConstants.ts
+++
@@ -1,5 +0,0 @@
-export const MATCH_REQUEST = "MATCH_REQUEST";
-export const MATCH_SUCCESS = "MATCH_SUCCESS";
-export const MATCH_FAIL = "MATCH_FAIL";
-export const MATCH_LIVE_SUCCESS = "MATCH_LIVE_SUCCESS";
-export const CLEAR_ERRORS = "CLEAR_ERRORS";
