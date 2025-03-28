143d66d4b11d1512e8be04084c68df73b505eeca
rajeshrajesh <rajeshmn47@gmail.com>
2025-03-28 09:26:55 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -131,7 +131,7 @@ export function Navbar({ home }) {
         </Account>
         <Center>
           <EmojiIcon />
-          Power11
+          FC4U
         </Center>
         <RightSide>
           <NotificationAddOutlinedIcon
