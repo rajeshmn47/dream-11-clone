4d61734e23befd8719312ce8d5bc57cef0bdcfff
rajeshmn47 <rajeshmn47@gmail.com>
2025-05-02 20:08:23 +0530

    adding working backend url

--- src/constants/userConstants.js
+++ src/constants/userConstants.js
@@ -78,7 +78,8 @@ function geturl() {
   // return 'http://46.28.44.4:8000';
   // return "https://fantasycricket4u.com";
   // return "https://dreamelevenclonebackend-g4iw.onrender.com"
-  return "https://backend.dreambet11.io";
+  // return "https://backend.dreambet11.io";
+  return "https://gamizo.duckdns.org";
 }
 
 function getfrontendurl() {
