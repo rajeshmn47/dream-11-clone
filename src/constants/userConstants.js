afe3daa33cc19a5bd550ef59b47466e46a051fde
rajeshmn47 <rajeshmn47@gmail.com>
2025-09-19 06:56:29 +0530

    fix

--- src/constants/userConstants.js
+++ src/constants/userConstants.js
@@ -59,7 +59,7 @@ function geturl() {
     // return "http://dream-env.eba-6d6ds7up.us-east-1.elasticbeanstalk.com";
     // return "https://backendforpuand-dream11.onrender.com";
     // return 'https://apk.thepowerplay.co';
-    // return 'http://localhost:8000';
+    return 'http://localhost:8000';
     // return 'http://192.168.237.175:8000';
     // return 'http://46.28.44.4:8000';
     // return "https://dreamelevenclonebackend-g4iw.onrender.com";
@@ -67,7 +67,7 @@ function geturl() {
     // return "https://backend.dreambet11.io";
     // return "https://gamizo.duckdns.org";
     // return "http://localhost:9000";
-     return "https://mangogames.fun/api"
+    // return "https://mangogames.fun/api"
   }
   // return "https://dreamelevenclonebackend-7l34.onrender.com";
   // return "https://dreamelevenclonebackend-1v8e.onrender.com";
@@ -83,7 +83,7 @@ function geturl() {
   // return "https://dreamelevenclonebackend-g4iw.onrender.com"
   // return "https://backend.dreambet11.io";
   // return "https://gamizo.duckdns.org";
-   return "https://mangogames.fun/api"
+  return "https://fango11.duckdns.org"
 }
 
 function getfrontendurl() {
