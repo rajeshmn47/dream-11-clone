850454f9acf865aa26700e273f3690d1ca77510b
rajeshmn47 <rajeshmn47@gmail.com>
2025-11-26 15:36:34 +0530

    fix

--- src/components/helpAndSupport.js
+++ src/components/helpAndSupport.js
@@ -15,51 +15,9 @@ const helpAndSupport = () => (
     <div className="help-and-support">
       <h1>Contact Us</h1>
 
-      <p>You may contact us using the information below:</p>
-
-      <ul>
-        <li>
-          <p>
-            <strong>Merchant Legal entity name:</strong>
-            {' '}
-            KARTAMI FINTECH PRIVATE
-            LIMITED
-          </p>
-        </li>
-        <li>
-          <p>
-            <strong>Registered Address:</strong>
-            {' '}
-            universal trade tower, sector 49, gurgaon, PIN: 122018
-          </p>
-        </li>
-        <li>
-          <p>
-            <strong>Operational Address:</strong>
-            {' '}
-            universal trade tower, sector 49, gurgaon, PIN: 122018
-          </p>
-        </li>
-        <li>
-          <p>
-            <strong>Email ID:</strong>
-            {' '}
-            <a href="mailto:info@dreamcricket11.com">info@dreamcricket11.com</a>
-          </p>
-        </li>
-      </ul>
-
-      <p>Thank you for reaching out to us!</p>
-      <div style={{ display: 'flex', marginTop: "135px", flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-        <p>
-          <strong>Designed and Developed by:</strong>
-          {' '}
-        </p>
-        <p>
-          KARTAMI FINTECH PRIVATE
-          LIMITED
-        </p>
-      </div>
+      <p>Contact Us
+        Mail : admin@gmail.com
+      </p>
     </div>
   </div>
 );
